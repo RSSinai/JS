10f6968aeb53daa27b5d768cb269ecdcc196de53
Rony Sinai <rony.sinai@gmail.com>
2023-09-26 18:45:10 +0300

    5.4 assignmant

---
+++ 5.4.js
@@ -0,0 +1,24 @@
+let language = prompt("please write a color");
+language = language.toLowerCase();
+switch (language) {
+  case "yellow":
+    console.log("light color");
+    break;
+  case "pink":
+    console.log("light color");
+    break;
+  case "orange":
+    console.log("light color");
+    break;
+  case "blue":
+    console.log("dark color");
+    break;
+  case "purple":
+    console.log("dark color");
+    break;
+  case "brown":
+    console.log("dark color");
+    break;
+  default:
+    console.log("Unknown color");
+}
