6c7c2fa12f0a7c9a6911a8ffae5cbad9c80e4a2a
Rony Sinai <rony.sinai@gmail.com>
2023-10-04 13:33:02 +0300

    2.1

---
+++ exc/8.5.js
@@ -0,0 +1,13 @@
+// Create a function that receives 1 argument, an object, and returns a new object with the properties and values swapped
+obj = {
+    build: "today",
+    sucks: "tomorrow"
+}
+
+
+function swap(obj)
+{
+    console.log(obj.entries()
+    )}
+
+    swap(obj)
