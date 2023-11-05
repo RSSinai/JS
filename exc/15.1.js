b680e877c1d458c2ebebf306fe15c0ba36521d2d
Rony Sinai <rony.sinai@gmail.com>
2023-11-05 21:42:54 +0200

    adding 15.1

--- exc/15.1.js
+++ exc/15.1.js
@@ -1,13 +1,13 @@
-function getSum(arr1, arr2){
-const sum = 0;
-for (let i=0; i < arr1.length; i++){
-sum += arr1[i];
+function getSum(arr1, arr2) {
+  let sum = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    sum += arr1[i];
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    sum += arr2[i];
+  }
 }
-for (let i=0; i < arr2.length; i++){
-sum += arr2[i];
-}
-}
-getSum([1,2,3][5,66,23]);
+getSum([1, 2, 3], [5, 66, 23]);
 
 // 1. line 10 has problem, line 2 has problem
 // 2. with the output log
