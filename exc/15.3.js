221cc95e98035f3826cc87a467dd55a8aa64589f
Rony Sinai <rony.sinai@gmail.com>
2023-11-06 13:51:12 +0200

    15.3

---
+++ exc/15.3.js
@@ -0,0 +1,9 @@
+function getSumOfEven(arr){
+    return arr[2] + arr[4] + arr[6] + arr[8];
+    }
+    console.log(getSumOfEven([1,2,3,4,5,6,7,8,9,10]))
+
+    // First, find the line that contains the problem. Write it down. - line 2
+    // 2. Which debug method did you use to find the bug? - i used breakpoints
+    // 3. Explain the bug in your own words. - theres no index number 10
+    // 4. Fix the code and submit it all.
