949a1698daea67b89149afdda1e9494ff0fc3196
Rony Sinai <rony.sinai@gmail.com>
2023-11-15 14:28:04 +0200

    adding 23.2

---
+++ exc/23.1.js
@@ -0,0 +1,2 @@
+//1. this will point to the active window object.. with all its properties and methods, the reason for this is because its in the global scope 
+//2. 
---
+++ weekend_10_11/js/cartJS.js
@@ -0,0 +1,34 @@
+const cartStorage = localStorage.getItem("myCartArray");
+const cart = JSON.parse(cartStorage);
+
+const cartContainer = document.getElementById("cart-cards");
+
+cart.forEach((flight) => {
+  const card = document.createElement("div");
+  card.classList.add("flight-card");
+  card.innerHTML = `
+  
+        <h2>${flight.id}</h2>
+        <p>From: ${flight.from}</p>
+        <p>To: ${flight.to}</p>
+        <p>Price: ${flight.price}$</p>
+    `;
+  cartContainer.appendChild(card);
+});
+
+const prices = cart.map((flight) => flight.price);
+console.log(prices);
+const totalPrice = prices.reduce((sum, price) => sum + price, 0);
+
+//
+let numberInput = document.getElementById("myNumberInput");
+
+let clientAmount = numberInput.addEventListener("input", function () {
+let inputValue = numberInput.value;
+document.getElementById("cart-sum").innerHTML = inputValue * totalPrice + "$";
+});
+
+function confirmCart(){
+    alert('The flights you selected are confirmed, an email will be sent')
+}
+
